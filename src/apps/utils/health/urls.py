06cb01f0bda8bd030be67d10ff3443a1f62e7516
DiceNameIsMy <dicenameismy@gmail.com>
2021-12-15 19:04:59 +0600

    refactor: health app urls

--- src/apps/utils/health/urls.py
+++ src/apps/utils/health/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import HealthCheckView
 
 urlpatterns = [
-    path('health/', HealthCheckView.as_view(), name='health'),
+    path('', HealthCheckView.as_view(), name='health'),
 ]
--- src/config/urls.py
+++ src/config/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include([
-        path('', include('apps.utils.health.urls')),
+        path('health/', include('apps.utils.health.urls')),
 
         path('v1/', include([
             path('accounts/', include('apps.accounts.api.v1.urls')),
